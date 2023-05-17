24ddb144b9257912f534273c76dda20036828de1
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-17 21:56:45 +0000

    Finished all index.php errors

--- index.php
+++ index.php
@@ -59,19 +59,19 @@
     // if userInteger is equal to an empty string or is less than 0, ask them to enter a positive integer
     if (($userInteger == "") || ($userInteger < 0)) {
         $numbers = "Please enter a positive integer.";
-    }
-    // otherwise, use a do...while loop to calculate the factorial of userInteger
-    else {
-           do {
+    }else {
+      // otherwise, use a do...while loop to calculate the factorial of userInteger
+        do {
             // calculate the factorial by multiplying the factorial variable by the counter plus one
             $factorial = $factorial * ($counter + 1);
 
             // increment the counter
             $counter = $counter + 1;
-           } while ($counter < $userInteger);
-      // define the numbers statement
-      $numbers = $userInteger . "! = " . $factorial;
+        } while ($counter < $userInteger);
+        // define the numbers statement
+        $numbers = $userInteger . "! = " . $factorial;
     }
+
     // display the factorial
     echo $numbers;
     ?>
