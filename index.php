324f4f1379c663ed686c3b093162a7e12750e968
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-05-17 21:20:05 +0000

    Added results.php code to index.php iframe

--- index.php
+++ index.php
@@ -44,48 +44,38 @@
       <!--Display Statement for Museum Admission-->
       <iframe id="result" name="result">
  <?php
-    // Created by: Julien Lamoureux
-    // Created on: May 2023
-    // This file contains the JS functions for index.html, Unit5-02-PHP-ProductOfNumbers
+  // initialize numbers to an empty string
+  $numbers = "";
 
-    // intialize product to 0
-    $product = 0;
+  // initialize counter to 0
+  $counter = 0;
 
-    // initialize numbers to an empty string
-    $numbers = "";
+  // initialize factorial to 1
+  $factorial = 1;
 
-    // get first integer
-    $firstInt = $_POST["first-int"];
-    $int1 = intval($firstInt);
-    $absFirstInt = abs($int1);
+  // get userInteger from user input
+  $userInteger = $_POST["first-int"];
 
-    // get second integer
-    $secondInt = $_POST["second-int"];
-    $int2 = intval($secondInt);
-    $absSecondInt = abs($int2);
+  // if userInteger is equal to an empty string or is less than 0, ask them to enter a positive integer
+if (($userInteger == "") || ($userInteger < 0)) {
+     $numbers = "Please enter a positive integer.";
+}
+  // otherwise, use a do...while loop to calculate the factorial of userInteger
+else {
+    do {
+       // calculate the factorial by multiplying the factorial variable by the counter plus one
+        $factorial = $factorial*($counter+1);
 
-    // if the user enters nothing, tell them to enter two integers
-    if (($firstInt == "") || ($secondInt == "")) {
-        echo "Please enter two integers.";
-    } else {
-    // use a for loop to calculate the product from the two integers
-        for ($counter = 0; $counter < $absSecondInt; $counter++) {
-            $product += $absFirstInt;
-        }
+       // increment the counter
+        $counter = $counter + 1;
+     } while ($counter < $userInteger);
 
-    // if the firstInt is less than 0 and the secondInt is greater than 0, then make the product negative
-        if (($firstInt < 0) && ($secondInt > 0)) {
-            $product = $product * -1;
-        } elseif (($secondInt < 0) && ($firstInt > 0)) {
-    // otherwise, if firstInt is greater than 0 and secondInt is less than 0, then make the product negative
-            $product = $product * -1;
-        }
-    // redefine calculation statement
-        $numbers = $firstInt . "*" . $secondInt . "=" . $product;
-    }
-
-    // display the product
-    echo $numbers;
+  // define the numbers statement
+    $numbers = $userInteger . "! = " . $factorial;
+  }
+  
+// display the factorial
+  echo $numbers;
     ?>
     </iframe>
       <!-- MDL Progress Bar with Indeterminate Progress -->
